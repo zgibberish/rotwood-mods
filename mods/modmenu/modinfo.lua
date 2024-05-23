16f19cb4d72e38e0cb22a723f05a29c1519c0739
gibberish <lamgiabao1456@gmail.com>
2024-05-24 01:28:25 +0700

    Update modinfo.lua
    Updated modmenu version, I almost forgot to do that.

--- mods/modmenu/modinfo.lua
+++ mods/modmenu/modinfo.lua
@@ -1,7 +1,7 @@
 name = "Mod Menu"
 description = "GUI to view and manage installed mods, adds a \"Mods\" tab located in the game options screen.\n<i>WARNING: If you disable the mod menu itself from the mods page, you will lose access to the GUI to enable it again. If you need to re-enable this mod, either do \"KnownModIndex:Enable(\"modmenu\") KnownModIndex:Save()\" in the debug console and reload, or, you can remove the mod from the mods/ folder and place it back in again, because the mod loader automatically enables newly installed mods. This also works for any other mods.</i>"
 author = "gibberish"
-version = "1.2-testing"
+version = "1.2"
 api_version = 10
 
 dst_compatible = false
